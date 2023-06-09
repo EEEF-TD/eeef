9044e084c51d754d427b322caaf47f6a60e1f552
EEEF-TD <136036371+EEEF-TD@users.noreply.github.com>
2023-06-08 20:57:04 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update -y && apt upgrade -y
+sudo apt install figlet -y
+sudo git clone https://github.com/xero/figlet-fonts.git 
+sudo mv figlet-fonts /usr/share/figlet-fonts
+sudo apt install git -y
+sudo apt install cmatrix -y
+sudo apt install wget -y
+sudo chmod +x eeef -y
+sudo mv eeef /usr/bin/
+sudo apt install toilet -y
+sudo apt install lolcat -y
+sudo apt install cowsay -y
+sudo apt install gay -y
+sudo gem install lolcat
+sudo pip install wordlist
